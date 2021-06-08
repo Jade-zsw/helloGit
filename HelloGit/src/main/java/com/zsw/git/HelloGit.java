634a567815d6dea9e6d57603ad22f354dc4a750b
张绍炜 <1804412136@qq.com>
2021-06-08 19:40:32 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/zsw/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.zsw.git;
+
+/**
+ * @author 张绍炜
+ * @create 2021-06-2021/6/8-19:24
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
